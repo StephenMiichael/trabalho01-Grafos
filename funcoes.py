d78d008b4aede91057fc356d6319cbb09959eacf
Stephen Michael <stephenmichaela@gmail.com>
2020-09-03 18:02:42 -0300

    Adicionado mais verificações de entrada de usuários

--- funcoes.py
+++ funcoes.py
@@ -42,14 +42,29 @@ def arestas(matriz):
 
             elif escolha == 2:
                 while True:
-                    os.system('cls')
-                    v1 = int(input('Origem: '))
-                    v2 = int(input('Destino: '))
-                    matriz[v1-1][v2-1] = 0
-                    n = int(input('Continuar ? [1]Sim [0]Nao '))
-                    if n == 0:
+                    try:
                         os.system('cls')
-                        break
+                        v1 = int(input('Origem: '))
+                        v2 = int(input('Destino: '))
+                        matriz[v1-1][v2-1] = 0
+                        n = int(input('Continuar ? [1]Sim [0]Nao '))
+                        if n == 0:
+                            os.system('cls')
+                            break
+                        if n == 1:
+                            continue
+                        else:
+                            os.system('cls')
+                            print(f'Você selecionou uma opção inválida ({n}), e por isso, interromperemos a exclusão de arestas/arcos.\n')
+                            os.system('pause')
+                            os.system('cls')
+                            break
+                    except ValueError:
+                        os.system('cls')
+                        print("Isso não é um número, interromperemos a exclusão de arestas/arcos.\n")
+                        os.system('pause')
+                        os.system('cls')
+                        arestas(matriz)
 
             elif escolha == 3:
                 os.system('cls')
@@ -57,12 +72,12 @@ def arestas(matriz):
 
             else:
                 os.system('cls')
-                print("Este número não está nas alternativas, tente novamente :D.\n")
+                print("Opção inválida. Tente novamente.\n")
                 os.system('pause')
                 os.system('cls')
         except ValueError:
             os.system('cls')
-            print("Isso não é um número, tente novamente :D.\n")
+            print("Opção inválida. Tente novamente.\n")
             os.system('pause')
             os.system('cls')
             arestas(matriz)
@@ -73,40 +88,78 @@ def arestas(matriz):
 def vertices(matriz):
 
     while True:
-        os.system('cls')
-        escolha = int(input("""Opções de vértices:
-              1 - Incluir vertices
-              2 - Excluir vertices
-              3 - Sair 
-     Escolha: """))
-
-        if escolha == 1:
-            while True:
-                os.system('cls')
-                v1 = int(input('Incluir vertice: '))
-                #matriz[v1 - 1][v2 - 1] = 1
-                n = int(input('Continuar ? [1]Sim [0]Nao '))
-                if n == 0:
-                    os.system('cls')
-                    break
-
-        elif escolha == 2:
-            while True:
-                os.system('cls')
-                v1 = int(input('Excluir vertice: '))
-                matriz = np.delete(matriz, (v1 - 1), axis=0)
-                matriz = np.delete(matriz, (v1 - 1), axis=1)
-                n = int(input('Continuar ? [1]Sim [0]Nao '))
-                if n == 0:
-                    os.system('cls')
-                    break
-
-        elif escolha == 3:
+        try:
             os.system('cls')
-            break
+            escolha = int(input("""Opções de vértices:
+                1 - Incluir vertices
+                2 - Excluir vertices
+                3 - Sair 
+        Escolha: """))
 
-        else:
+            if escolha == 1:
+                while True:
+                    try:
+                        os.system('cls')
+                        v1 = int(input('Incluir vertice: '))
+                        #matriz[v1 - 1][v2 - 1] = 1
+                        n = int(input('Continuar ? [1]Sim [0]Nao '))
+                        if n == 0:
+                            os.system('cls')
+                            break
+                        elif n == 1:
+                            continue
+                        else:
+                            os.system('cls')
+                            print(f'Você selecionou uma opção inválida ({n}), e por isso, interromperemos a inclusão de vértices.\n')
+                            os.system('pause')
+                            os.system('cls')
+                            break
+                    except ValueError:
+                        os.system('cls')
+                        print("Opção inválida. Tente novamente.\n")
+                        os.system('pause')
+                        os.system('cls')
+                        vertices(matriz)
+                    
+            elif escolha == 2:
+                try:
+                    while True:
+                        os.system('cls')
+                        v1 = int(input('Excluir vertice: '))
+                        matriz = np.delete(matriz, (v1 - 1), axis=0)
+                        matriz = np.delete(matriz, (v1 - 1), axis=1)
+                        n = int(input('Continuar ? [1]Sim [0]Nao '))
+                        if n == 0:
+                            os.system('cls')
+                            break
+                        elif n == 1:
+                            continue
+                        else:
+                            os.system('cls')
+                            print(f'Você selecionou uma opção inválida ({n}), e por isso, interromperemos a inclusão de vértices.\n')
+                            os.system('pause')
+                            os.system('cls')
+                            break
+                except ValueError:
+                        os.system('cls')
+                        print("Isso não é um número, interromperemos a exclusão de vertices.\n")
+                        os.system('pause')
+                        os.system('cls')
+                        vertices(matriz) 
+
+            elif escolha == 3:
+                os.system('cls')
+                break
+            else:
+                os.system('cls')
+                print("Opção inválida. Tente novamente.\n")
+                os.system('pause')
+                os.system('cls')
+        except ValueError:
             os.system('cls')
-            print("Este número não está nas alternativas, tente novamente :D.\n")
+            print("Opção inválida. Tente novamente.\n")
+            os.system('pause')
+            os.system('cls')
+            vertices(matriz)
 
     return matriz
